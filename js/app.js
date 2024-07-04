4f38ce164ec1b5389c71605f8870d0b21ede5535
Gui_SamuelBS <153688693+Gu1zinh0@users.noreply.github.com>
2024-07-04 00:08:44 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,556 @@
+/* btnPict.addEventListener('click', () => {
+
+     if (img.style.display === 'block') {
+        img.style.display = 'none'
+     } else {
+        img.style.display = 'block'
+     }
+ })*/
+ 
+ /*  btnPict.addEventListener('mouseenter', () => {
+    
+   img.style.display = 'block'
+})
+
+btnPict.addEventListener('mouseout', () => {
+    img.style.display = 'none'
+})*/
+
+//Atletismo
+const btnPictAtl = document.querySelector('#athletic')
+const imgAth = document.querySelector('.pict_athl')
+
+btnPictAtl.addEventListener('mouseenter', () => {
+    
+   imgAth.style.display = 'block'
+})
+
+btnPictAtl.addEventListener('mouseout', () => {
+    imgAth.style.display = 'none'
+})
+
+//Badminton
+const btnPictBad = document.querySelector('#badminton')
+const imgBad = document.querySelector('.pict_bad')
+
+btnPictBad.addEventListener('mouseenter', () => {
+    
+    imgBad.style.display = 'block'
+ })
+ 
+ btnPictBad.addEventListener('mouseout', () => {
+     imgBad.style.display = 'none'
+ })
+
+//Basquete
+const btnPictBask = document.querySelector('#basquete')
+const imgBasq = document.querySelector('.pict_bas')
+
+btnPictBask.addEventListener('mouseenter', () => {
+    
+    imgBasq.style.display = 'block'
+ })
+ 
+ btnPictBask.addEventListener('mouseout', () => {
+     imgBasq.style.display = 'none'
+ })
+
+//Basquete 3x3
+const btnPictBaskt = document.querySelector('#baskt')
+const imgBaskt = document.querySelector('.pict_baskt')
+
+btnPictBaskt.addEventListener('mouseenter', () => {
+    
+    imgBaskt.style.display = 'block'
+ })
+ 
+btnPictBaskt.addEventListener('mouseout', () => {
+     imgBaskt.style.display = 'none'
+})
+
+//Boxe
+const btnPictBoxe = document.querySelector('#boxe')
+const imgBoxe = document.querySelector('.pict_boxe')
+
+btnPictBoxe.addEventListener('mouseenter', () => {
+    
+    imgBoxe.style.display = 'block'
+ })
+ 
+ btnPictBoxe.addEventListener('mouseout', () => {
+     imgBoxe.style.display = 'none'
+ })
+
+//Breakdance
+const btnPictBreak = document.querySelector('#breakdance')
+const imgBreak =document.querySelector('.pict_break')
+
+btnPictBreak.addEventListener('mouseenter', () => {
+    
+    imgBreak.style.display = 'block'
+ })
+ 
+ btnPictBreak.addEventListener('mouseout', () => {
+     imgBreak.style.display = 'none'
+ })
+
+//Canoagem
+const btnPictCanoagem = document.querySelector('#canoagem')
+const imgCanoagem = document.querySelector('.pict_canoagem')
+
+btnPictCanoagem.addEventListener('mouseenter', () => {
+    
+    imgCanoagem.style.display = 'block'
+ })
+ 
+ btnPictCanoagem.addEventListener('mouseout', () => {
+     imgCanoagem.style.display = 'none'
+ })
+
+//Canoagem Slalom
+const btnPictSlalom = document.querySelector('#slalom')
+const imgSlalom = document.querySelector('.pict_slalom')
+
+btnPictSlalom.addEventListener('mouseenter', () => {
+    
+    imgSlalom.style.display = 'block'
+ })
+ 
+btnPictSlalom.addEventListener('mouseout', () => {
+    imgSlalom.style.display = 'none'
+})
+
+//Ciclismo BMX
+const btnPictBmx = document.querySelector('#bmx')
+const imgBmx = document.querySelector('.pict_bmx')
+
+btnPictBmx.addEventListener('mouseenter', () => {
+    
+    imgBmx.style.display = 'block'
+ })
+ 
+btnPictBmx.addEventListener('mouseout', () => {
+    imgBmx.style.display = 'none'
+})
+
+//Ciclismo Freestylle
+const btnPictFrestyle = document.querySelector('#frestyle')
+const imgFrestyle = document.querySelector('.pict_frestyle')
+
+btnPictFrestyle.addEventListener('mouseenter', () => {
+    
+    imgFrestyle.style.display = 'block'
+ })
+ 
+btnPictFrestyle.addEventListener('mouseout', () => {
+    imgFrestyle.style.display = 'none'
+})
+
+//Ciclismo de Montanha
+const btnPictMountain = document.querySelector('#mountain')
+const imgMountain = document.querySelector('.pict_mountain')
+
+btnPictMountain.addEventListener('mouseenter', () => {
+    
+    imgMountain.style.display = 'block'
+ })
+ 
+btnPictMountain.addEventListener('mouseout', () => {
+    imgMountain.style.display = 'none'
+})
+
+//Ciclismo de Pista
+const btnPictPista = document.querySelector('#pista')
+const imgPista = document.querySelector('.pict_pista')
+
+btnPictPista.addEventListener('mouseenter', () => {   
+    imgPista.style.display = 'block'
+ })
+ 
+btnPictPista.addEventListener('mouseout', () => {
+    imgPista.style.display = 'none'
+})
+
+//Ciclismo de Rua
+const btnPictRua = document.querySelector('#rua')
+const imgRua = document.querySelector('.pict_rua')
+
+btnPictRua.addEventListener('mouseenter', () => {   
+    imgRua.style.display = 'block'
+ })
+ 
+btnPictRua.addEventListener('mouseout', () => {
+    imgRua.style.display = 'none'
+})
+
+//Escalada
+const btnPictEscalada = document.querySelector('#escalada')
+const imgEscalada = document.querySelector('.pict_escalada')
+
+btnPictEscalada.addEventListener('mouseenter', () => {   
+    imgEscalada.style.display = 'block'
+ })
+ 
+btnPictEscalada.addEventListener('mouseout', () => {
+    imgEscalada.style.display = 'none'
+})
+
+//Esgrima
+const btnPictEsgrima = document.querySelector('#esgrima')
+const imgEsgrima = document.querySelector('.pict_esgrima')
+
+btnPictEsgrima.addEventListener('mouseenter', () => {   
+    imgEsgrima.style.display = 'block'
+ })
+ 
+btnPictEsgrima.addEventListener('mouseout', () => {
+    imgEsgrima.style.display = 'none'
+})
+
+//Futebol
+const btnPictFutebol = document.querySelector('#futebol')
+const imgFutebol = document.querySelector('.pict_futebol')
+
+btnPictFutebol.addEventListener('mouseenter', () => {   
+    imgFutebol.style.display = 'block'
+ })
+ 
+btnPictFutebol.addEventListener('mouseout', () => {
+    imgFutebol.style.display = 'none'
+})
+
+//Gins. Artística
+const btnPictArtistica = document.querySelector('#artistica')
+const imgArtistica =document.querySelector('.pict_artistica')
+
+btnPictArtistica.addEventListener('mouseenter', () => {   
+    imgArtistica.style.display = 'block'
+ })
+ 
+btnPictArtistica.addEventListener('mouseout', () => {
+    imgArtistica.style.display = 'none'
+})
+
+//Trampolim
+const btnPictTrampolim = document.querySelector('#trampolim')
+const imgTrampolim = document.querySelector('.pict_trampolim')
+
+btnPictTrampolim.addEventListener('mouseenter', () => {   
+    imgTrampolim.style.display = 'block'
+ })
+ 
+btnPictTrampolim.addEventListener('mouseout', () => {
+    imgTrampolim.style.display = 'none'
+})
+
+//Gins. Rítmica
+const btnPictRitmica = document.querySelector('#ritmica')
+const imgRitmica = document.querySelector('.pict_ritmica')
+
+btnPictRitmica.addEventListener('mouseenter', () => {
+    imgRitmica.style.display = 'block'
+ })
+ 
+btnPictRitmica.addEventListener('mouseout', () => {
+    imgRitmica.style.display = 'none'
+})
+
+//Golfe
+const btnPictGolfe = document.querySelector('#golfe')
+const imgGolfe = document.querySelector('.pict_golfe')
+
+btnPictGolfe.addEventListener('mouseenter', () => {
+    imgGolfe.style.display = 'block'
+ })
+ 
+btnPictGolfe.addEventListener('mouseout', () => {
+    imgGolfe.style.display = 'none'
+})
+
+//Handebol
+const btnPictHandebol = document.querySelector('#handebol')
+const imgHandebol = document.querySelector('.pict_handebol')
+
+btnPictHandebol.addEventListener('mouseenter', () => {
+    imgHandebol.style.display = 'block'
+ })
+ 
+btnPictHandebol.addEventListener('mouseout', () => {
+    imgHandebol.style.display = 'none'
+})
+
+//Hipismo
+const btnPictHipismo = document.querySelector('#hipismo')
+const imgHipismo = document.querySelector('.pict_hipismo')
+
+btnPictHipismo.addEventListener('mouseenter', () => {
+    imgHipismo.style.display = 'block'
+ })
+ 
+btnPictHipismo.addEventListener('mouseout', () => {
+    imgHipismo.style.display = 'none'
+})
+
+//Hockey
+const btnPictHockey = document.querySelector('#hockey')
+const imgHockey = document.querySelector('.pict_hockey')
+
+btnPictHockey.addEventListener('mouseenter', () => {
+    imgHockey.style.display = 'block'
+ })
+ 
+btnPictHockey.addEventListener('mouseout', () => {
+    imgHockey.style.display = 'none'
+})
+
+//Judô
+const btnPictJudo = document.querySelector('#judo')
+const imgJudo = document.querySelector('.pict_judo')
+
+btnPictJudo.addEventListener('mouseenter', () => {
+    imgJudo.style.display = 'block'
+ })
+ 
+btnPictJudo.addEventListener('mouseout', () => {
+    imgJudo.style.display = 'none'
+})
+
+//Levantamento
+const btnPictLevantamento = document.querySelector('#levantamento')
+const imgLevantamento = document.querySelector('.pict_levantamento')
+
+btnPictLevantamento.addEventListener('mouseenter', () => {
+    imgLevantamento.style.display = 'block'
+ })
+ 
+btnPictLevantamento.addEventListener('mouseout', () => {
+    imgLevantamento.style.display = 'none'
+})
+
+//Lutas
+const btnPictLutas = document.querySelector('#lutas')
+const imgLutas = document.querySelector('.pict_lutas')
+
+btnPictLutas.addEventListener('mouseenter', () => {
+    imgLutas.style.display = 'block'
+ })
+ 
+btnPictLutas.addEventListener('mouseout', () => {
+    imgLutas.style.display = 'none'
+})
+
+//Nado
+const btnPictNado = document.querySelector('#nado')
+const imgNado = document.querySelector('.pict_nado')
+
+btnPictNado.addEventListener('mouseenter', () => {
+    imgNado.style.display = 'block'
+ })
+ 
+btnPictNado.addEventListener('mouseout', () => {
+    imgNado.style.display = 'none'
+})
+
+//Natação
+const btnPictNatacao = document.querySelector('#natacao')
+const imgNatacao = document.querySelector('.pict_natacao')
+
+btnPictNatacao.addEventListener('mouseenter', () => {
+    imgNatacao.style.display = 'block'
+ })
+ 
+btnPictNatacao.addEventListener('mouseout', () => {
+    imgNatacao.style.display = 'none'
+})
+
+//Pentatlo
+const btnPictPentatlo = document.querySelector('#pentatlo')
+const imgPentatlo = document.querySelector('.pict_pentatlo')
+
+btnPictPentatlo.addEventListener('mouseenter', () => {
+    imgPentatlo.style.display = 'block'
+ })
+ 
+btnPictPentatlo.addEventListener('mouseout', () => {
+    imgPentatlo.style.display = 'none'
+})
+
+//Polo
+const btnPictPolo = document.querySelector('#polo')
+const imgPolo = document.querySelector('.pict_polo')
+
+btnPictPolo.addEventListener('mouseenter', () => {
+    imgPolo.style.display = 'block'
+ })
+ 
+btnPictPolo.addEventListener('mouseout', () => {
+    imgPolo.style.display = 'none'
+})
+
+//Remo
+const btnPictRemo = document.querySelector('#remo')
+const imgRemo = document.querySelector('.pict_remo')
+
+btnPictRemo.addEventListener('mouseenter', () => {
+    imgRemo.style.display = 'block'
+ })
+ 
+btnPictRemo.addEventListener('mouseout', () => {
+    imgRemo.style.display = 'none'
+})
+
+//Rugby
+const btnPictRugby = document.querySelector('#rugby')
+const imgRugby = document.querySelector('.pict_rugby')
+
+btnPictRugby.addEventListener('mouseenter', () => {
+    imgRugby.style.display = 'block'
+ })
+ 
+btnPictRugby.addEventListener('mouseout', () => {
+    imgRugby.style.display = 'none'
+})
+
+//Saltos
+const btnPictSaltos = document.querySelector('#saltos')
+const imgSaltos = document.querySelector('.pict_saltos')
+
+btnPictSaltos.addEventListener('mouseenter', () => {
+    imgSaltos.style.display = 'block'
+ })
+ 
+btnPictSaltos.addEventListener('mouseout', () => {
+    imgSaltos.style.display = 'none'
+})
+
+//Skate
+const btnPictSkate = document.querySelector('#skate')
+const imgSkate = document.querySelector('.pict_skate')
+
+btnPictSkate.addEventListener('mouseenter', () => {
+    imgSkate.style.display = 'block'
+ })
+ 
+btnPictSkate.addEventListener('mouseout', () => {
+    imgSkate.style.display = 'none'
+})
+
+//Surfe
+const btnPictSurfe = document.querySelector('#surfe')
+const imgSurfe = document.querySelector('.pict_surfe')
+
+btnPictSurfe.addEventListener('mouseenter', () => {
+    imgSurfe.style.display = 'block'
+ })
+ 
+btnPictSurfe.addEventListener('mouseout', () => {
+    imgSurfe.style.display = 'none'
+})
+
+//Taekwondo
+const btnPictTaekwondo = document.querySelector('#taekwondo')
+const imgTaekwondo = document.querySelector('.pict_taekwondo')
+
+btnPictTaekwondo.addEventListener('mouseenter', () => {
+    imgTaekwondo.style.display = 'block'
+ })
+ 
+btnPictTaekwondo.addEventListener('mouseout', () => {
+    imgTaekwondo.style.display = 'none'
+})
+
+//Tenis
+const btnPictTenis = document.querySelector('#tenis')
+const imgTenis = document.querySelector('.pict_tenis')
+
+btnPictTenis.addEventListener('mouseenter', () => {
+    imgTenis.style.display = 'block'
+ })
+ 
+btnPictTenis.addEventListener('mouseout', () => {
+    imgTenis.style.display = 'none'
+})
+
+//Tênis de Mesa
+const btnPictTenisMesa = document.querySelector('#tenis_mesa')
+const imgTenisMesa = document.querySelector('.pict_tenis_mesa')
+
+btnPictTenisMesa.addEventListener('mouseenter', () => {
+    imgTenisMesa.style.display = 'block'
+ })
+ 
+btnPictTenisMesa.addEventListener('mouseout', () => {
+    imgTenisMesa.style.display = 'none'
+})
+
+//Tiro com Arco
+const btnPictArco = document.querySelector('#arco')
+const imgArco = document.querySelector('.pict_arco')
+
+btnPictArco.addEventListener('mouseenter', () => {
+    imgArco.style.display = 'block'
+ })
+ 
+btnPictArco.addEventListener('mouseout', () => {
+    imgArco.style.display = 'none'
+})
+
+//Tiro Esportivo
+const btnPictEsportivo = document.querySelector('#esportivo')
+const imgEsportivo = document.querySelector('.pict_esportivo')
+
+btnPictEsportivo.addEventListener('mouseenter', () => {
+    imgEsportivo.style.display = 'block'
+ })
+ 
+btnPictEsportivo.addEventListener('mouseout', () => {
+    imgEsportivo.style.display = 'none'
+})
+
+//Triatlo
+const btnPictTriatlo = document.querySelector('#triatlo')
+const imgTriatlo = document.querySelector('.pict_triatlo')
+
+btnPictTriatlo.addEventListener('mouseenter', () => {
+    imgTriatlo.style.display = 'block'
+ })
+ 
+btnPictTriatlo.addEventListener('mouseout', () => {
+    imgTriatlo.style.display = 'none'
+})
+
+//Vela
+const btnPictVela = document.querySelector('#vela')
+const imgVela = document.querySelector('.pict_vela')
+
+btnPictVela.addEventListener('mouseenter', () => {
+    imgVela.style.display = 'block'
+ })
+ 
+btnPictVela.addEventListener('mouseout', () => {
+    imgVela.style.display = 'none'
+})
+
+//Volei
+const btnPictVolei = document.querySelector('#volei')
+const imgVolei = document.querySelector('.pict_volei')
+
+btnPictVolei.addEventListener('mouseenter', () => {
+    imgVolei.style.display = 'block'
+ })
+ 
+btnPictVolei.addEventListener('mouseout', () => {
+    imgVolei.style.display = 'none'
+})
+
+//Volei de Praia
+const btnPictVoleiPraia = document.querySelector('#volei_praia')
+const imgVoleiPraia = document.querySelector('.pict_volei_praia')
+
+btnPictVoleiPraia.addEventListener('mouseenter', () => {
+    imgVoleiPraia.style.display = 'block'
+ })
+ 
+btnPictVoleiPraia.addEventListener('mouseout', () => {
+    imgVoleiPraia.style.display = 'none'
+})
---
+++ js/script.js
@@ -0,0 +1,315 @@
+/*const btnPict = document.querySelector('#') 
+const img = document.querySelector('.')
+
+btnPict.addEventListener('mouseenter', () => {
+    
+   img.style.display = 'block'
+})
+
+btnPict.addEventListener('mouseout', () => {
+    img.style.display = 'none'
+})*/
+
+//Atletismo
+const btnPictAthlP = document.querySelector('#atletismo-p');
+const imgAthlP = document.querySelector('.pict_atletismo-p');
+
+btnPictAthlP.addEventListener('mouseenter', () => {
+    imgAthlP.style.display = 'block'
+});
+
+btnPictAthlP.addEventListener('mouseout', () => {
+    imgAthlP.style.display = 'none'
+});
+ 
+
+//Badminton
+const btnPictBadP = document.querySelector('#badminton-p') 
+const imgBadP = document.querySelector('.pict_bad-p')
+
+btnPictBadP.addEventListener('mouseenter', () => {
+    
+   imgBadP.style.display = 'block'
+})
+
+btnPictBadP.addEventListener('mouseout', () => {
+    imgBadP.style.display = 'none'
+})
+
+
+//Basquete
+const btnPictBasP = document.querySelector('#basquete-p') 
+const imgBasP = document.querySelector('.pict_bas-p')
+
+btnPictBasP.addEventListener('mouseenter', () => {
+    
+   imgBasP.style.display = 'block'
+})
+
+btnPictBasP.addEventListener('mouseout', () => {
+    imgBasP.style.display = 'none'
+})
+
+
+//Bocha
+const btnPictBocha = document.querySelector('#bocha') 
+const imgBocha = document.querySelector('.pict_bocha')
+
+btnPictBocha.addEventListener('mouseenter', () => {
+    
+   imgBocha.style.display = 'block'
+})
+
+btnPictBocha.addEventListener('mouseout', () => {
+    imgBocha.style.display = 'none'
+})
+
+
+//Canoagem
+const btnPictCanoagemP = document.querySelector('#canoagem-p') 
+const imgCanoagemP = document.querySelector('.pict_canoagem-p')
+
+btnPictCanoagemP.addEventListener('mouseenter', () => {
+    
+   imgCanoagemP.style.display = 'block'
+})
+
+btnPictCanoagemP.addEventListener('mouseout', () => {
+    imgCanoagemP.style.display = 'none'
+})
+
+
+//Ciclismo de Rua
+const btnPictRuaP = document.querySelector('#ciclismo_estrada-p')
+const imgRuaP = document.querySelector('.pict_rua-p')
+
+btnPictRuaP.addEventListener('mouseenter', () => {
+    
+    imgRuaP.style.display = 'block'
+ })
+ 
+ btnPictRuaP.addEventListener('mouseout', () => {
+     imgRuaP.style.display = 'none'
+ })
+
+//Ciclismo de Pista
+const btnPictPistaP = document.querySelector('#ciclismo_pista-p')
+const imgPistaP = document.querySelector('.pict_pista-p') 
+
+btnPictPistaP.addEventListener('mouseenter', () => {
+    
+    imgPistaP.style.display = 'block'
+ })
+ 
+ btnPictPistaP.addEventListener('mouseout', () => {
+     imgPistaP.style.display = 'none'
+ })
+
+//Esgrima
+const btnPictEsgrimaP = document.querySelector('#esgrima-p')
+const imgEsgrimaP = document.querySelector('.pict_esgrima-p')
+
+btnPictEsgrimaP.addEventListener('mouseenter', () => {
+    
+    imgEsgrimaP.style.display = 'block'
+ })
+ 
+ btnPictEsgrimaP.addEventListener('mouseout', () => {
+     imgEsgrimaP.style.display = 'none'
+ })
+
+//Futebol de Cegos
+const btnPictFutebolCegos = document.querySelector('#futebol-cegos')
+const imgFutebolCegos = document.querySelector('.pict_futebol-cegos')
+
+btnPictFutebolCegos.addEventListener('mouseenter', () => {
+    
+    imgFutebolCegos.style.display = 'block'
+ })
+ 
+ btnPictFutebolCegos.addEventListener('mouseout', () => {
+     imgFutebolCegos.style.display = 'none'
+ })
+
+//Goalball
+const btnPictGoalball = document.querySelector('#goalball')
+const imgGoalball = document.querySelector('.pict_goalball')
+
+btnPictGoalball.addEventListener('mouseenter', () => {
+    
+    imgGoalball.style.display = 'block'
+ })
+ 
+ btnPictGoalball.addEventListener('mouseout', () => {
+     imgGoalball.style.display = 'none'
+ })
+
+//Halterofilismo
+const btnPictHalterofilismoP = document.querySelector('#halterofilismo-p')
+const imgHalterofilismoP = document.querySelector('.pict_halterofilismo')
+
+btnPictHalterofilismoP.addEventListener('mouseenter', () => {
+    
+    imgHalterofilismoP.style.display = 'block'
+ })
+ 
+ btnPictHalterofilismoP.addEventListener('mouseout', () => {
+     imgHalterofilismoP.style.display = 'none'
+ })
+
+//Hipismo
+const btnPictHipismoP = document.querySelector('#hipismo-p')
+const imgHipismoP = document.querySelector('.pict_hipismo-p')
+
+btnPictHipismoP.addEventListener('mouseenter', () => {
+    
+    imgHipismoP.style.display = 'block'
+ })
+ 
+ btnPictHipismoP.addEventListener('mouseout', () => {
+     imgHipismoP.style.display = 'none'
+ })
+
+//Judô
+const btnPictJudoP = document.querySelector('#judo-p')
+const imgJudoP = document.querySelector('.pict_judo-p')
+
+btnPictJudoP.addEventListener('mouseenter', () => {
+    
+    imgJudoP.style.display = 'block'
+ })
+ 
+ btnPictJudoP.addEventListener('mouseout', () => {
+     imgJudoP.style.display = 'none'
+ })
+
+//Natação
+const btnPictNatacaoP = document.querySelector('#natacao-p')
+const imgNatacaoP = document.querySelector('.pict_natacao-p')
+
+btnPictNatacaoP.addEventListener('mouseenter', () => {
+    
+    imgNatacaoP.style.display = 'block'
+ })
+ 
+ btnPictNatacaoP.addEventListener('mouseout', () => {
+     imgNatacaoP.style.display = 'none'
+ })
+
+//Remo
+const btnPictRemoP = document.querySelector('#remo-p')
+const imgRemoP = document.querySelector('.pict_remo-p')
+
+btnPictRemoP.addEventListener('mouseenter', () => {
+    
+    imgRemoP.style.display = 'block'
+ })
+ 
+ btnPictRemoP.addEventListener('mouseout', () => {
+     imgRemoP.style.display = 'none'
+ })
+
+//Rugby
+const btnPictRugbyP = document.querySelector('#rugby-p')
+const imgRugbyP = document.querySelector('.pict_rugby-p')
+
+btnPictRugbyP.addEventListener('mouseenter', () => {
+    
+    imgRugbyP.style.display = 'block'
+ })
+ 
+ btnPictRugbyP.addEventListener('mouseout', () => {
+     imgRugbyP.style.display = 'none'
+ })
+
+//Taekwondo
+const btnPictTaekwondoP = document.querySelector('#taekwondo-p')
+const imgTaekwondoP = document.querySelector('.pict_taekwondo-p')
+
+btnPictTaekwondoP.addEventListener('mouseenter', () => {
+    
+    imgTaekwondoP.style.display = 'block'
+ })
+ 
+ btnPictTaekwondoP.addEventListener('mouseout', () => {
+     imgTaekwondoP.style.display = 'none'
+ })
+
+//Tenis de Mesa
+const btnPictMesaP = document.querySelector('#tenis_mesa-p')
+const imgMesaP = document.querySelector('.pict_tenis_mesa-p')
+
+btnPictMesaP.addEventListener('mouseenter', () => {
+    
+    imgMesaP.style.display = 'block'
+ })
+ 
+ btnPictMesaP.addEventListener('mouseout', () => {
+     imgMesaP.style.display = 'none'
+ })
+
+//Tenis
+const btnPictTenisP = document.querySelector('#tenis-p')
+const imgTenisP = document.querySelector('.pict_tenis-p')
+
+btnPictTenisP.addEventListener('mouseenter', () => {
+    
+    imgTenisP.style.display = 'block'
+ })
+ 
+ btnPictTenisP.addEventListener('mouseout', () => {
+     imgTenisP.style.display = 'none'
+ })
+
+//Tiro Arco
+const  btnPictArcoP = document.querySelector('#arco-p')
+const imgArcoP = document.querySelector('.pict_arco-p')
+
+btnPictArcoP.addEventListener('mouseenter', () => {
+    
+    imgArcoP.style.display = 'block'
+ })
+ 
+ btnPictArcoP.addEventListener('mouseout', () => {
+     imgArcoP.style.display = 'none'
+ })
+
+//Tiro Esportivo
+const btnPictEsportivoP = document.querySelector('#esportivo-p')
+const imgEsportivoP = document.querySelector('.pict_esportivo-p')
+
+btnPictEsportivoP.addEventListener('mouseenter', () => {
+    
+    imgEsportivoP.style.display = 'block'
+ })
+ 
+ btnPictEsportivoP.addEventListener('mouseout', () => {
+     imgEsportivoP.style.display = 'none'
+ })
+
+//Triatlo
+const btnPictTriatloP = document.querySelector('#triatlo-p')
+const imgTriatloP = document.querySelector('.pict_triatlo-p')
+
+btnPictTriatloP.addEventListener('mouseenter', () => {
+    
+    imgTriatloP.style.display = 'block'
+ })
+ 
+ btnPictTriatloP.addEventListener('mouseout', () => {
+     imgTriatloP.style.display = 'none'
+ })
+
+//Volei Sentado
+const btnPictVoleiS = document.querySelector('#volei_sentado')
+const imgVoleiS = document.querySelector('.pict_volei_sentado')
+
+btnPictVoleiS.addEventListener('mouseenter', () => {
+    
+    imgVoleiS.style.display = 'block'
+ })
+ 
+ btnPictVoleiS.addEventListener('mouseout', () => {
+     imgVoleiS.style.display = 'none'
+ })
+
